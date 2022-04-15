12de7139e02f227b41c3294708f002107b0fc0e4
Arseniy Khvorov <khvorov45@gmail.com>
2022-04-15 10:27:14 +1000

    use msvc

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 @echo off
 
-clang-cl -Od -Z7 -nologo -c -TC -D_CRT_SECURE_NO_WARNINGS -Fobuild\tinyfiledialog.obj .\code\tinyfiledialogs\tinyfiledialogs.c
-llvm-lib -nologo build\tinyfiledialog.obj -out:build\tinyfiledialog.lib
+cl -Od -Z7 -nologo -c -TC -D_CRT_SECURE_NO_WARNINGS -Fobuild\tinyfiledialog.obj .\code\tinyfiledialogs\tinyfiledialogs.c
+lib -nologo build\tinyfiledialog.obj -out:build\tinyfiledialog.lib
 
 odin build code -out:build/wiredeck.exe -debug -default-to-nil-allocator
