0bb3e9e2a68ebfb18043c21946dc90b04d38a1de
Arseniy Khvorov <khvorov45@gmail.com>
2022-07-01 10:21:10 +1000

    comment out build

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 @echo off
 
-odin build code -out:build/wiredeck.exe -debug -default-to-nil-allocator -no-crt
+rem odin build code -out:build/wiredeck.exe -debug -default-to-nil-allocator -no-crt
 
-odin run tests -out:build/tests.exe -debug
+rem odin run tests -out:build/tests.exe -debug
