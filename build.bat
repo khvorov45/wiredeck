8edd075c8ab326200d96babd956839cbc9267fa0
Arseniy Khvorov <khvorov45@gmail.com>
2022-07-03 11:34:27 +1000

    check shell.bat exists

--- build.bat
+++ build.bat
@@ -1,10 +1,6 @@
 @echo off
 
-rem odin build code -out:build/wiredeck.exe -debug -default-to-nil-allocator -no-crt
-
-rem odin run tests -out:build/tests.exe -debug
-
-call shell.bat
+if exist shell.bat call shell.bat
 
 cl /nologo /Zi /diagnostics:column ^
 	/D_CRT_SECURE_NO_WARNINGS /DPLATFORM_WINDOWS /W3 ^
