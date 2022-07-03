20c74987b6c8632fd005487b3c12ac7fb959e5ef
Arseniy Khvorov <khvorov45@gmail.com>
2022-07-03 11:36:57 +1000

    add cl check

--- build.bat
+++ build.bat
@@ -2,6 +2,11 @@
 
 if exist shell.bat call shell.bat
 
+where /q cl || (
+  echo ERROR: "cl" not found - please run this from the "MSVC x64 native tools command prompt" or create shell.bat to set up the environment.
+  exit /b 1
+)
+
 cl /nologo /Zi /diagnostics:column ^
 	/D_CRT_SECURE_NO_WARNINGS /DPLATFORM_WINDOWS /W3 ^
 	/Fdbuild.pdb ^
